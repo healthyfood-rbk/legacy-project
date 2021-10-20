f0b6910fd7932a2429a8fdf3eb17f2b35c104519
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-10-20 17:41:50 +0100

    admin0.1

--- legay-project/src/app/app.module.ts
+++ legay-project/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AddFoodComponent } from './admin/add-food-List/add-food.component';
+import { CardComponent } from './admin/card/card.component';
+import { ButtonAddComponent } from './admin/button-add/button-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AddFoodComponent,
+    CardComponent,
+    ButtonAddComponent
   ],
   imports: [
     BrowserModule
