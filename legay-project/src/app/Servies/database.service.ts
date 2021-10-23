f4136cd428e545654f641b949ec9df285daa6ebf
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-10-23 11:13:11 +0100

    admin and delete and update

--- legay-project/src/app/Servies/database.service.ts
+++ legay-project/src/app/Servies/database.service.ts
@@ -2,18 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { CardComponent } from '../admin/card/card.component';
+const baseUrl = 'http://localhost:3000'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
   
-    path : string = 'http://localhost:3000/'
     constructor(private http: HttpClient) { 
     }
     
  fetch () : Observable<any>{
-  return  this.http.get<any>("./api/admin")
+  return  this.http.get(baseUrl+"/api/admin")
  }
+//  update(id, data): Observable<any> {
+//   return this.http.put(`${baseUrl}/${id}`, data);
+// }
+delete(id:any): Observable<any> {
+    return this.http.delete(`${baseUrl}/api/admin/${id}`);
+  }
+
 
 }
