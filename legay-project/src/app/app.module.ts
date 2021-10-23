f0f4c21fa6eee0d766680d2986a974a874ca27fd
oussema-yahyaoui <ousema.benabdallah.yahyaoui@gmail.com>
2021-10-23 11:39:49 +0100

    to pull

--- legay-project/src/app/app.module.ts
+++ legay-project/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+
 import { AdminComponent } from './admin/admin.component';
 import { AddFoodComponent } from './admin/add-food-List/add-food.component';
 import { CardComponent } from './admin/card/card.component';
@@ -18,6 +19,11 @@ import {AuthModule} from '@auth0/auth0-angular'
 import {environment as env } from '../environments/environment';
 import { LogoutButtonComponent } from './auth-components/logout-button/logout-button.component';
 import { FiltredDataComponent } from './filtred-data/filtred-data.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
+
+
 
 
 @NgModule({
@@ -36,6 +42,10 @@ import { FiltredDataComponent } from './filtred-data/filtred-data.component';
     LoginButtonComponent,
     LogoutButtonComponent,
     FiltredDataComponent,
+    HeaderComponent,
+    CartComponent,
+
+  
 
   ],
   imports: [
@@ -44,7 +54,8 @@ import { FiltredDataComponent } from './filtred-data/filtred-data.component';
     HttpClientModule,
     AuthModule.forRoot({
       ...env.auth,
-    })
+    }),
+    AppRoutingModule
 
   ],
   providers: [],
