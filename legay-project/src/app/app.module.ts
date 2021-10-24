12543fb63256a787e168f75ec4bece9e306d00d6
oussema-yahyaoui <ousema.benabdallah.yahyaoui@gmail.com>
2021-10-24 19:12:04 +0100

    add to card

--- legay-project/src/app/app.module.ts
+++ legay-project/src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-import { AddFoodComponent } from './admin/add-food-List/add-food.component';
-import { CardComponent } from './admin/card/card.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FoodCardsComponent } from './food-cards/food-cards.component';
@@ -13,22 +11,18 @@ import { PaymentComponent } from './payment/payment.component';
 import { AdminSignupComponent } from './auth-components/admin-signup/admin-signup.component';
 
 import { LoginButtonComponent } from './auth-components/login-button/login-button.component';
-import {AuthModule} from '@auth0/auth0-angular'
-import {environment as env } from '../environments/environment';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from '../environments/environment';
 import { LogoutButtonComponent } from './auth-components/logout-button/logout-button.component';
 import { FiltredDataComponent } from './filtred-data/filtred-data.component';
-import {UpdateComponent} from './admin/update/update.component'
-import {FooterOneComponent} from './footer/footer.component'
-console.log(document.cookie.length)
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AdminModule } from './admin/admin.module';
+import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-
-    AdminComponent,
-    AddFoodComponent,
-    CardComponent,
-   
-
+    routingComponents,
     NavbarComponent,
     FoodCardsComponent,
     PaymentComponent,
@@ -36,30 +30,30 @@ console.log(document.cookie.length)
     LoginButtonComponent,
     LogoutButtonComponent,
     FiltredDataComponent,
-    UpdateComponent,
-    FooterOneComponent
+    AddToCartComponent,
   ],
   imports: [
+    // AdminModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path : "" , component:FoodCardsComponent},
-      {path : "details" , component:FiltredDataComponent},
-      {path : "admin" , component:AdminComponent},
-      {path : "addfood", component: AddFoodComponent},
-      {path : "updatefood", component: UpdateComponent},
 
+    AppRoutingModule,
+
+    RouterModule.forRoot([
+      { path: '', component: FoodCardsComponent },
+      { path: 'details', component: FiltredDataComponent },
+      { path: 'cart', component: AddToCartComponent },
       // {path : "**" , component:},
       // {path : "details" , component:FiltredDataComponent},
-
     ]),
+
     AuthModule.forRoot({
       ...env.auth,
-    })
-
+    }),
+    // AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
